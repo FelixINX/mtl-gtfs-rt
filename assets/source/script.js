7c062956b57e0b9cecab7cf525e5548fbd60b2c3
Félix Desjardins <felixinx@gmail.com>
2018-12-09 19:39:59 -0500

    It's christmas time!

--- assets/source/script.js
+++ assets/source/script.js
@@ -1,6 +1,6 @@
 // map preparation
 
-var mtl_gtfsrt_map = L.map('map').setView([45.5576, -73.7242], 11.5);
+var mtl_gtfsrt_map = L.map('leaflet-map').setView([45.5576, -73.7242], 11.5);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -15,6 +15,8 @@ var transitLayer = L.layerGroup().addTo(mtl_gtfsrt_map);
 // when page is loaded
 
 $(function () {
+    loadRandomImage(imageURLs);
+
     loadTransit("stm1" , "#00a54f");
     loadTransit("stm2" , "#f58220");
     loadTransit("stm4" , "#ffdc01");
@@ -29,7 +31,8 @@ $(function () {
     loadData(dataUrl);
 
     // when the page has finish loading, show the map
-    $('#mainTab li:first-child a').tab('show');
+    $('.loading').css('display', 'none');
+    $('#leaflet-map').css('opacity', '1');
 
     function showOnMap(lat, lon) {
         mtl_gtfsrt_map.setView([lat, lon], 18);
@@ -45,13 +48,29 @@ $(function () {
 
     function loadData(data_source){
         $.get(data_source, function (data, pos_status) {
+
+            function randomSanta (obj) {
+                var keys = Object.keys(obj);
+                return obj[keys[keys.length * Math.random() << 0]];
+            }
+
+            var random = randomSanta(data.results);
+
+
             // display vehicles on the map
             data.results.forEach(function (element) {
-                L.marker(L.latLng(element.lat, element.lon), {
-                    icon: icons[element.icon]
-                }).addTo(posLayer)
-                    .bindPopup("<h3>" + element.agency + " " + element.vehicle_id + "</h3><b>Trip: </b> " + element.trip_id + "<br><b>Route: </b>" + element.route_id + "<br><b>Start: </b>" + element.start_date + " " + element.start_time + "<br><b>Current stop sequence: </b>" + element.current_stop_sequence + "<br><b>Status: </b>" + element.current_status);
-            });
+                if (element.vehicle_id === random.vehicle_id) {
+                    L.marker(L.latLng(random.lat, random.lon), {
+                        icon: trainOrBus(random.agency)
+                    }).addTo(posLayer)
+                        .bindPopup("<h3>" + random.agency + " " + random.vehicle_id + "</h3><b>Trip: </b> " + random.trip_id + "<br><b>Route: </b>" + random.route_id + "<br><b>Start: </b>" + random.start_date + " " + random.start_time + "<br><b>Current stop sequence: </b>" + random.current_stop_sequence + "<br><b>Status: </b>" + random.current_status, {'className':'santa-popup'});
+                } else {
+                    L.marker(L.latLng(element.lat, element.lon), {
+                        icon: icons[element.icon]
+                    }).addTo(posLayer)
+                        .bindPopup("<h3>" + element.agency + " " + element.vehicle_id + "</h3><b>Trip: </b> " + element.trip_id + "<br><b>Route: </b>" + element.route_id + "<br><b>Start: </b>" + element.start_date + " " + element.start_time + "<br><b>Current stop sequence: </b>" + element.current_stop_sequence + "<br><b>Status: </b>" + element.current_status);
+                }
+                });
             // load vehicles in the list
             var dataTable = $('#data-table').DataTable({
                 data: data.results,
@@ -100,6 +119,23 @@ $(function () {
             }).addTo(transitLayer);
         });
     }
+
+    function trainOrBus(agency) {
+        if (agency == "exo") {
+            return icons["christmasTrainIcon"]
+        } else {
+            return icons["christmasBusIcon"]
+        }
+    }
+
+    function loadRandomImage(URLsArray) {
+        var randomIndex = Math.floor(Math.random() * URLsArray.length);
+        var css = 'url(';
+        css += URLsArray[randomIndex];
+        css += ') center center no-repeat';
+        $(".main-bg").css('background', css);
+        $(".main-bg").css('background-size', 'cover');
+    }
 });
 
 // global variables
@@ -112,7 +148,21 @@ var icons = {
     "trainExoIcon": L.icon({
         iconUrl: 'https://felixinx.github.io/mtl-gtfs-rt/assets/map-train-exo.svg',
         iconSize: [20, 20]
+    }),
+    "christmasBusIcon": L.icon({
+        iconUrl: 'http://localhost/christmas/assets/map-christmas-bus.svg',
+        iconSize: [87, 50]
+    }),
+    "christmasTrainIcon": L.icon({
+        iconUrl: 'http://localhost/christmas/assets/map-christmas-train-short.svg',
+        iconSize: [148, 43]
     })
 };
 
 var dataUrl = 'https://mtl-gtfs-rt-backend.azurewebsites.net/data/latest.json';
+
+var imageURLs = [
+    "assets/images/joy-real-535919-unsplash-min.jpg",
+    "assets/images/joy-real-587637-unsplash-min.jpg",
+    "assets/images/nicolae-rosu-555257-unsplash-min.jpg"
+];
