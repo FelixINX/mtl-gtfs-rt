9e842a2231ddbecc766079de6f814539f30ee972
Félix Desjardins <FelixINX@users.noreply.github.com>
2020-10-27 16:59:12 -0400

    Update to exo URL and removed STM (temp)

--- rt.php
+++ rt.php
@@ -15,7 +15,7 @@ if ($app_apikey_header == $app_apikey) {
 }
 
 // set headers for STM GTFS realtime request
-$exo_feed_url = "http://opendata.rtm.quebec:2539/ServiceGTFSR/VehiclePosition.pb?token=" . getenv('EXO_APIKEY');
+$exo_feed_url = "https://opendata.exo.quebec/ServiceGTFSR/VehiclePosition.pb?token=" . getenv('EXO_APIKEY');
 $stm_feed_url = "https://api.stm.info/pub/od/gtfs-rt/ic/v1/vehiclePositions";
 $stm_apikey_header = "apikey: " . getenv('STM_APIKEY');
 $stm_opts = [
@@ -27,23 +27,24 @@ $stm_opts = [
 $stm_context = stream_context_create($stm_opts);
 
 $exo_data = file_get_contents($exo_feed_url);
-$stm_data = file_get_contents($stm_feed_url, false, $stm_context);
+// $stm_data = file_get_contents($stm_feed_url, false, $stm_context);
 $exo_feed = new FeedMessage();
-$stm_feed = new FeedMessage();
+// $stm_feed = new FeedMessage();
 $exo_feed->parse($exo_data);
-$stm_feed->parse($stm_data);
+// $stm_feed->parse($stm_data);
 
 $timestamp_exo = (string)$exo_feed->header->getTimestamp();
-$timestamp_stm = (string)$stm_feed->header->getTimestamp();
+// $timestamp_stm = (string)$stm_feed->header->getTimestamp();
 
 $data = array(
   "time_exo" => $timestamp_exo,
-  "time_stm" => $timestamp_stm,
+  "time_stm" => 0,
+//   "time_stm" => $timestamp_stm,
   "results" => array()
 );
 
 // download raw data
-file_put_contents("data/raw/vehiclePosition.pb", $stm_data);
+// file_put_contents("data/raw/vehiclePosition.pb", $stm_data);
 
 // add exo data to array
 foreach ($exo_feed->getEntityList() as $entity) {
@@ -68,39 +69,40 @@ foreach ($exo_feed->getEntityList() as $entity) {
 }
 
 // add STM data to array
-foreach ($stm_feed->getEntityList() as $entity) {
-    $vehicle = $entity->getId();
-    $route = $entity->vehicle->trip->getRouteId();
-    $trip = $entity->vehicle->trip->getTripId();
-    $start_time = $entity->vehicle->trip->getStartTime();
-    $start_date = $entity->vehicle->trip->getStartDate();
-    $current_stop_sequence = $entity->vehicle->getCurrentStopSequence();
-    $current_status_number = $entity->vehicle->getCurrentStatus();
-    if ($current_status_number == 1) {
-      $current_status = "STOPPED_AT";
-    } elseif ($current_status_number == 2) {
-      $current_status = "IN_TRANSIT_TO";
-    }
-    $lat = round($entity->vehicle->position->getLatitude(), 5);
-    $lon = round($entity->vehicle->position->getLongitude(), 5);
-    $data['results'][] = array(
-    'agency' => 'STM',
-    'icon' => 'busSTMIcon',
-    'vehicle_id' => $vehicle,
-    'route_id' => $route,
-    'trip_id' => $trip,
-    'start_time' => $start_time,
-    'start_date' => $start_date,
-    'current_stop_sequence' => $current_stop_sequence,
-    'current_status' => $current_status,
-    'lat' => $lat,
-    'lon' => $lon
-  );
-}
+// foreach ($stm_feed->getEntityList() as $entity) {
+//     $vehicle = $entity->getId();
+//     $route = $entity->vehicle->trip->getRouteId();
+//     $trip = $entity->vehicle->trip->getTripId();
+//     $start_time = $entity->vehicle->trip->getStartTime();
+//     $start_date = $entity->vehicle->trip->getStartDate();
+//     $current_stop_sequence = $entity->vehicle->getCurrentStopSequence();
+//     $current_status_number = $entity->vehicle->getCurrentStatus();
+//     if ($current_status_number == 1) {
+//       $current_status = "STOPPED_AT";
+//     } elseif ($current_status_number == 2) {
+//       $current_status = "IN_TRANSIT_TO";
+//     }
+//     $lat = round($entity->vehicle->position->getLatitude(), 5);
+//     $lon = round($entity->vehicle->position->getLongitude(), 5);
+//     $data['results'][] = array(
+//     'agency' => 'STM',
+//     'icon' => 'busSTMIcon',
+//     'vehicle_id' => $vehicle,
+//     'route_id' => $route,
+//     'trip_id' => $trip,
+//     'start_time' => $start_time,
+//     'start_date' => $start_date,
+//     'current_stop_sequence' => $current_stop_sequence,
+//     'current_status' => $current_status,
+//     'lat' => $lat,
+//     'lon' => $lon
+//   );
+// }
 
 $csv_handle = fopen("data/count.csv", "a");
 $exo_count = (string)count($exo_feed->getEntityList());
-$stm_count = (string)count($stm_feed->getEntityList());
+$stm_count = "0";
+// $stm_count = (string)count($stm_feed->getEntityList());
 $csv_array = array($timestamp_exo, $timestamp_stm, $exo_count, $stm_count);
 fputcsv($csv_handle, $csv_array);
 fclose($csv_handle);
